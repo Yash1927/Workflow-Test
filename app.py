84f32872bdfa826d898f20d5e67d8f83a44d8d85
yash1927 <yashrastogi44444@gmail.com>
2024-09-08 18:10:37 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+fig = plt.figure()
+ax = fig.add_subplot()
+data = np.random.standard_normal(30).cumsum()
+ax.plot(data, color="black", label = "Default")
+ax.plot(data, color ="black", linestyle="dashed", drawstyle = "steps-post", label = "steps-post")
+ax.legend()
+plt.show()
