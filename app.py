0fd84fcdf9efb963bc5f6b1c63e0eb16f7546256
yash1927 <yashrastogi44444@gmail.com>
2024-09-08 18:54:30 +0530

    test

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
 fig = plt.figure()
 ax = fig.add_subplot()
-
-# Generating data
 data = np.random.standard_normal(30).cumsum()
-
-# Plot with incorrect parameters (e.g., an undefined color)
-ax.plot(data, color="undefined-color", label="Error")
-
+ax.plot(data, color="black", label = "Default")
+ax.plot(data, color ="black", linestyle="dashed", drawstyle = "steps-post", label = "steps-post")
 ax.legend()
 plt.show()
-print(shsss)
